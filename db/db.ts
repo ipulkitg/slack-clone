5d4c6da05641d2e2a6e131085777c71a904b199d
ipulkitg <pulkit1999@gmail.com>
2025-07-05 16:17:52 -0700

    init db

---
+++ db/db.ts
@@ -0,0 +1,19 @@
+import { config } from "dotenv"
+import { drizzle as drizzlePostgres } from "drizzle-orm/postgres-js"
+import postgres from "postgres"
+
+config({ path: ".env.local" })
+
+const databaseUrl = process.env.DATABASE_URL
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not set")
+}
+
+const dbSchema = {}
+
+function initializeDb(url: string) {
+  const client = postgres(url, { prepare: false })
+  return drizzlePostgres(client, { schema: dbSchema })
+}
+
+export const db = initializeDb(databaseUrl)
